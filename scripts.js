86bf4475888dead0fbda34a46d769ae59c22b8f0
sanorod <sanorod00@gmail.com>
2025-03-11 10:22:43 +0200

    chore: Added data set

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const recipes = [
+    {
+        title:"Spaghetti Bolognese",
+        ingredients: "Spaghetti, Ground Beef, Tomato Sauce, Parmesan Cheese",
+        steps: "Cook spaghetti, cook ground beef, combine with tomato sauce, top with parmes"
+    },
+    {
+        title:"Chicken Fajitas",
+        ingredients: "Chicken, Bell Peppers, Onions, Tortillas, Salsa",
+        steps: "Cook chicken, cook bell peppers and onions, serve with tortillas and salsa"
+    },
+    {
+        title:"Grilled Cheese Sandwich",
+        ingredients: "Bread, Cheese, Butter",
+        steps: "Butter bread, place cheese in between, grill until cheese is melted"
+    }
+
+];
