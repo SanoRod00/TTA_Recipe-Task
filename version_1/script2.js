56fbab08a332c3019b04587ca2d23e141e2a909d
sanorod <sanorod00@gmail.com>
2025-03-13 15:53:56 +0200

    Feature: Added editing functionality

--- version_1/script2.js
+++ version_1/script2.js
@@ -11,7 +11,7 @@ const displayRecipes = () => {
         <h2 class = "text-lg font-bold">${recipe.title}</h2>
         <p class = "text-sm text-gray-500"><strong class = "font-bold text-lg">Ingredients: &ensp;</strong>${recipe.ingredients}</p>
          <p class = "text-sm text-gray-500"><strong class = "font-bold text-lg">Steps: &ensp;</strong>${recipe.steps}</p>
-         <button class = "bg-green-500 text-white px-2 py-1 rounded mt-2">Edit</button>
+         <button class = "bg-green-500 text-white px-2 py-1 rounded mt-2" onClick = "editRecipe(${index})">Edit</button>
          <button class = "bg-red-500 text-white px-2 py-1 rounded mt-2" onClick = "deleteRecipe(${index})">Delete</button>
         `;
         recipeList.appendChild(recipeCard);
@@ -86,6 +86,21 @@ const isDuplicate = recipes.some((recipe) => recipe.title.toLowerCase() === reci
   }  
  }    
 }  
+const editRecipe = (index) => {
+
+    const updatedRecipeTitle = prompt('Enter the new recipe title', recipes[index].title);
+    const updatedRecipeIngredients = prompt('Enter the new recipe Ingredients', recipes[index].ingredients);
+    const updatedRecipeSteps = prompt('Enter the new recipe Steps', recipes[index].steps);
+
+    if(updatedRecipeTitle && updatedRecipeIngredients && updatedRecipeSteps){
+        recipes[index].title = updatedRecipeTitle;
+        recipes[index].ingredients = updatedRecipeIngredients;
+        recipes[index].steps = updatedRecipeSteps;
+
+        saveRecipeToLocalStorage();
+        displayRecipes();
+    }
+}
 const deleteRecipe = (index) => {
     recipes.splice(index, 1);
     saveRecipeToLocalStorage();
